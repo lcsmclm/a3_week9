8e2a018ca4698680926520b87f6a19543e95dfed
Lucas McCallum <lucasmccallum@lucass-mbp.fc.ca>
2017-03-15 13:23:32 -0400

    roundabout build

--- js/master.js
+++ js/master.js
@@ -1,17 +1,15 @@
 (function(){
     var selectedCar, saveButton = document.querySelector('.fa-download').parentNode; //parentnode is the element's "wrapper" (whatever it is nested in)
 
-
-    if (window.localStorage.getItem('carPicked')) {
-        var data = window.localStorage.getItem('carPicked', selectedCar);
-
+function loadStuff(){
+    if (window.localStorage.getItem('savedCar')) {
+        var data = window.localStorage.getItem('savedCar', selectedCar);
         data = JSON.parse(data);
         renderCarInfo(data);
-
     }
+};
 
 
-//NPM website
 //expanded AJAX example
     $('.thumbInfo img').on('click', function () {
 
@@ -23,30 +21,29 @@
         }) // don't put a semicolon here so we can chain methods together
 
         .done(function(data){
-            console.log(data);
-
+            //console.log(data);
             if(data && data !== "null") {
                 selectedCar = data;
                 data = JSON.parse(data);
                 renderCarInfo(data);
-
             } else {
                 alert('your ajax call didn\'t work');
             }
-
         })
-
         .fail(function(ajaxCall, status, error){
             console.log(status, ", ", error);
             console.dir(ajaxCall);
         }); //terminate the ajax function
-
-
     });
 
     function renderCarInfo(car) {
+            var currentThumb = $('#' + car.model);
+            var animIndex = parseInt(currentThumb.data('roundaboutindex'), 8);
+            $('#cars').roundabout('animateToChild', animIndex);
+
             $('.thumbInfo img').addClass('nonActive');
             $('#' + car.model).removeClass('nonActive');
+
             $('.subhead span').text(" mini Cooper " + car.model);
             $('.modelName').text(car.modelName);
             $('.priceInfo').text(car.pricing);
@@ -55,12 +52,29 @@
 
     function saveData() {
         if(window.localStorage){
-        window.localStorage.setItem('carPicked', selectedCar);
+        window.localStorage.setItem('savedCar', selectedCar);
         }
     }
 
 
     saveButton.addEventListener('click', saveData, false);
+
+
+//jquery plugin
+
+    $(window).load(function(){
+        $('#cars').roundabout({
+            childSelector : 'img',
+            minOpacity : 0.8,
+            minScale : 0.4,
+            duration : 1200
+        });
+
+        $('#cars').css('opacity', 1);
+
+      loadStuff()
+    });
+
 
 
 })();
